d74e1171206d4a020bfb7a6284b03b524263d8b3
Wayne Landini <wlandini@mines.edu>
2012-12-04 15:42:57 -0700

    Adding timer screen

--- RouteOptimizer/src/csci498/wlandini/routeoptimizer/RouteOptimizer.java
+++ RouteOptimizer/src/csci498/wlandini/routeoptimizer/RouteOptimizer.java
@@ -2,7 +2,9 @@ package csci498.wlandini.routeoptimizer;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class RouteOptimizer extends Activity {
 
@@ -16,5 +18,10 @@ public class RouteOptimizer extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_route_optimizer, menu);
         return true;
+    }
+    
+    public void startNewPath(View view) {
+    	Intent i = new Intent(this, Timer.class);
+    	startActivity(i);
     }
 }
---
+++ RouteOptimizer/src/csci498/wlandini/routeoptimizer/Timer.java
@@ -0,0 +1,15 @@
+package csci498.wlandini.routeoptimizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Timer extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.timer_view);
+	}	
+	
+	
+}
